cbb6503549e656578c760df73cfabf5e0e6a0392
Siddy <dancore99@outlook.com>
2021-10-26 18:32:29 +0800

    index mostly complete test needs to be created

--- dist/export.js
+++ dist/export.js
@@ -1,5 +1,7 @@
-function generateemployee(response) {
-    return `
+const fs = require("fs");
+
+function topHtml() {
+    const htmlCode = `
     <!DOCTYPE html>
     <html lang="en">
     
@@ -16,23 +18,75 @@ function generateemployee(response) {
     
     <body>
         <div class="card-deck">
-            <div class="card border-success mb-3 .d-flex " style="max-width: 18rem;">
-                <div class="card-header text-white bg-primary border-success">${response.name} <br>
-                    ${response.qualification}</div>
-                <div class="card-body text-success">
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item">ID: ${response.id}</li>
-                        <li class="list-group-item">Email: ${repponse.email}</li>
-                        <li class="list-group-item">Office number: ${response.office} ${response.github} ${response.school}</li>
-                    </ul>
-                </div>
-                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-                    integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-                    crossorigin="anonymous"></script>
-    </body>
-    
-    </html>
-  `;
-  }
-  
-  module.exports = generateemployee;
+            <div class="card border-success mb-3 .d-flex " style="max-width: 18rem;">`;
+    fs.writeFile("./src/Team-Profile.html", html, function (err) {
+        if (err) {
+            console.log(err);
+        }
+    });
+    console.log("start");
+};
+
+function createCard(member){
+    return new Promise(function(resolve, reject) {
+        const name = member.getName();
+        const role = member.getRole();
+        const id = member.getId();
+        const email = member.getEmail();
+        let data = "";
+        if (role === "Engineer") {
+            const gitHub = member.getGithub(); 
+        const data = `
+        <div class="card-header text-white bg-primary border-success">${member.name} <br>
+        ${member.role}</div>
+        <div class="card-body text-success">
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">ID: ${member.id}</li>
+            <li class="list-group-item">Email: ${member.email}</li>
+            <li class="list-group-item">Office number: ${member.gitHub}</li>
+        </ul>
+        </div>`
+        } else if (role === "intern") {
+            const school = member.getSchool(); 
+        const data =`<div class="card-header text-white bg-primary border-success">${member.name} <br>
+        ${member.role}</div>
+        <div class="card-body text-success">
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">ID: ${member.id}</li>
+            <li class="list-group-item">Email: ${member.email}</li>
+            <li class="list-group-item">Office number: ${member.school}</li>
+        </ul>
+        </div>`
+        } else {
+            const officeNumber = member.getOfficeNumber();
+            const data =`<div class="card-header text-white bg-primary border-success">${member.name} <br>
+            ${member.role}</div>
+            <div class="card-body text-success">
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID: ${member.id}</li>
+                <li class="list-group-item">Email: ${member.email}</li>
+                <li class="list-group-item">Office number: ${member.officeNumber}</li>
+            </ul>
+            </div>`
+        }
+        console.log("adding team member");
+        fs.appendFile("./dist/Team-Profile.html", data, function (err) {
+            if (err) {
+                return reject(err);
+            };
+            return resolve();
+        });
+});
+}
+
+function bottomHtml(){
+    const htmlCode = 
+    `<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+    crossorigin="anonymous"></script>
+</body>
+
+</html>`
+}
+
+  module.exports = {topHtml, bottomHtml, createCard};
--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 const inquirer = require("inquirer");
 const fs = require('fs');
-const generateemployee = require("./dist/export.js");
+const htmlCreate = require("./dist/export");
+const Engineer = require("./lib/Engineer");
+const Employee = require("./lib/Employee");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
 
-const managerQuestions = [
+const employees = [];
+
+const employeeQuestions = [
     {
         type: "input",
         message: "Team managers name?",
         name: "name",
     },
     {
+        type: "list",
+        message:"What is your memebers role?",
+        choices:[
+            "Engineer",
+            "Intern",
+            "Manager"
+        ],
+        name:"role"
+    },
+    {
         type: "input",
-        message: "Employee number?",
+        message: "Employee ID?",
         name: "EmployeeNumber",
     },
     {
@@ -18,55 +34,57 @@ const managerQuestions = [
         Message: "Manager email?",
         name: "email",
     },
-    {
-        type: "input",
-        Message: "Office Number?",
-        name: "officeNumber",
-    },
 ];
 
-const engineerQuestions = [
-    {
-        type: "input",
-        message: "Engineers name?",
-        name: "name",
-    },
-    {
-        type: "input",
-        message: "Employee number?",
-        name: "EmployeeNumber",
-    },
-    {
-        type: "input",
-        Message: "Engineers email?",
-        name: "email",
-    },
-    {
-        type: "input",
-        Message: "Engineers Github?",
-        name: "Github",
-    },
-];
+function memberAdd(){
+    inquirer.prompt(employeeQuestions)
+    .then(function({name, role, id, email}) {
+        let info = "";
+        if (role === "Engineer") {
+            info = "GitHub username";
+        } else if (role === "Intern") {
+            info = "school";
+        } else {
+            info = "office number";
+        }
+        inquirer.prompt([{
+            message: `Enter team member's ${info}`,
+            name: "roleInfo"
+        },
+        {
+            type: "list",
+            message: "Would you like to add more team members?",
+            choices: [
+                "yes",
+                "no"
+            ],
+            name: "moreMembers"
+        }])
+        .then(function({info, moreMembers}) {
+            let newMember;
+            if (role === "Engineer") {
+                newMember = new Engineer(name, id, email, info);
+            } else if (role === "Intern") {
+                newMember = new Intern(name, id, email, info);
+            } else {
+                newMember = new Manager(name, id, email, info);
+            }
+            employees.push(newMember);
+            htmlCreate.createCard(newMember)
+            .then(function() {
+                if (moreMembers === "yes") {
+                    addMember();
+                } else {
+                    htmlCreate.bottomHtml();
+                }
+            });
+        })
+    })
+};
 
-const internQuestions = [
-    {
-        type: "input",
-        message: "Interns name?",
-        name: "name",
-    },
-    {
-        type: "input",
-        message: "Employee number?",
-        name: "EmployeeNumber",
-    },
-    {
-        type: "input",
-        Message: "Engineers email?",
-        name: "email",
-    },
-    {
-        type: "input",
-        Message: "Interns school?",
-        name: "school",
-    },
-];
+function init(){
+htmlCreate.topHtml
+memberAdd()
+};
+
+init()
